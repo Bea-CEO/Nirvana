1ad61cf169adb8818e00a2faf56915db31b8c810
Niklas Widmann <nik@somethingcatchy.net>
2025-06-03 11:39:26 +0200

    updated plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.possible-triangle.gradle") version ("0.2.7")
+    id("com.possible-triangle.gradle") version ("0.2.10")
 }
 
 subprojects {
--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,10 +1,9 @@
 val mc_version: String by extra
 val registrate_fabric_version: String by extra
-val jei_version: String by extra
 val mixin_extras_version: String by extra
 
 plugins {
-    id("dev.architectury.loom") version ("1.7-SNAPSHOT")
+    id("dev.architectury.loom") version ("1.10-SNAPSHOT")
 }
 
 common {
@@ -12,13 +11,12 @@ common {
 }
 
 dependencies {
-    "minecraft"("com.mojang:minecraft:${mc_version}")
-    "mappings"(loom.officialMojangMappings())
+   "minecraft"("com.mojang:minecraft:${mc_version}")
+   "mappings"(loom.officialMojangMappings())
 
     compileOnly("io.github.llamalad7:mixinextras-common:${mixin_extras_version}")
 
     modCompileOnly("com.tterrag.registrate_fabric:Registrate:${registrate_fabric_version}")
-    //modCompileOnly("mezz.jei:jei-${mc_version}-common-api:${jei_version}")
 }
 
 tasks.register("prepareWorkspace") {
