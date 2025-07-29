d7f1315c9b329df7db19973c78000516a9523820
Niklas Widmann <nik@somethingcatchy.net>
2025-07-29 10:08:36 +0200

    moving worldgen features to datagen

--- fabric/src/main/java/galena/nirvana/fabric/datagen/FabricDataGenerators.java
+++ fabric/src/main/java/galena/nirvana/fabric/datagen/FabricDataGenerators.java
@@ -24,6 +24,7 @@ public class FabricDataGenerators implements DataGeneratorEntrypoint {
         FabricEntrypoint.REGISTRATE.setupDatagen(pack, fileHelper);
         pack.addProvider(AdditionalNirvanaRecipes::new);
         pack.addProvider(PackMetadataProvider::new);
+        pack.addProvider(DataRegistries::new);
     }
 
     private void addDefaultTranslations() {
